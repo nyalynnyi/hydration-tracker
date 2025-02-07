a69f09dada35066e4cbab18afeb543f91e9dd1d4
Nikita Yalynnyi <n_yalynnyi@utb.cz>
2025-02-07 05:24:19 +0100

    drink history added

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { ThemeService } from '../theme.service';
-import { NavController } from '@ionic/angular';
-import { ActivatedRoute } from '@angular/router';
-import { AlertController } from '@ionic/angular';
 import { AppStorageService } from '../app-storage.service';
-import { WEIGHT, ACTIVE_MINUTES, WATER_GOAL } from '../app.constants';
+import { DRINK_HISTORY } from '../app.constants';
 
 
 @Component({
@@ -15,48 +11,30 @@ import { WEIGHT, ACTIVE_MINUTES, WATER_GOAL } from '../app.constants';
 })
 export class Tab3Page {
 
-  constructor(private themeService: ThemeService, private alertController: AlertController,
-    private appStorage: AppStorageService) {}
-  
-    async showInputPrompt() {
-      const alert = await this.alertController.create({
-        header: 'Enter Your Details',
-        inputs: [
-          { name: 'weightKg', type: 'number', placeholder: 'Weight (kg)' },
-          { name: 'activityMinutes', type: 'number', placeholder: 'Daily activity (minutes)' },
-          { name: 'hydrationGoal', type: 'number', placeholder: 'Water Goal (ml)' }
-        ],
-        buttons: [
-          {
-            text: 'OK',
-            handler: (data) => {
-              this.saveUserData(data);
-            },
-          },
-        ],
-      });
-  
-      await alert.present();
-    }
-  
-    saveUserData(data: any) {
-      const { weightKg, activityMinutes, hydrationGoal } = data;
-  
-      // Save the data to localStorage
-      localStorage.setItem(WEIGHT, String(weightKg));
-      localStorage.setItem(ACTIVE_MINUTES, String(activityMinutes));
-      localStorage.setItem(WATER_GOAL, String(hydrationGoal));
-  
-      // Update the values in the AppStorageService
-      this.appStorage.set(WEIGHT, weightKg);
-      this.appStorage.set(ACTIVE_MINUTES, activityMinutes);
-      this.appStorage.set(WATER_GOAL, hydrationGoal);
-    }
+  drinkHistory: { amount: number; timestamp: string }[] = [];
 
-  toggleTheme() {
-    this.themeService.toggleTheme();
+  constructor(private appStorage: AppStorageService) {}
+
+  async ionViewWillEnter() {
+    await this.loadDrinkHistory();
   }
 
-  
+  async loadDrinkHistory() {
+    const history = await this.appStorage.get(DRINK_HISTORY);
+    if (history) {
+      this.drinkHistory = JSON.parse(history);
+    }
+  }
+
+  async deleteDrink(index: number) {
+    this.drinkHistory.splice(index, 1); 
+    await this.appStorage.set(DRINK_HISTORY, JSON.stringify(this.drinkHistory)); 
+  }
 
 }
+
+
+
+
+
+
---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { AppStorageService } from '../app-storage.service';
+import { DRINK_HISTORY } from '../app.constants';
+
+import { ThemeService } from '../theme.service';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { WEIGHT, ACTIVE_MINUTES, WATER_GOAL } from '../app.constants';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
+  standalone: false,
+})
+export class Tab4Page {
+  constructor(private themeService: ThemeService, private alertController: AlertController,
+      private appStorage: AppStorageService) {}
+    
+      async showInputPrompt() {
+        const alert = await this.alertController.create({
+          header: 'Enter Your Details',
+          inputs: [
+            { name: 'weightKg', type: 'number', placeholder: 'Weight (kg)' },
+            { name: 'activityMinutes', type: 'number', placeholder: 'Daily activity (minutes)' },
+            { name: 'hydrationGoal', type: 'number', placeholder: 'Water Goal (ml)' }
+          ],
+          buttons: [
+            {
+              text: 'OK',
+              handler: (data) => {
+                this.saveUserData(data);
+              },
+            },
+          ],
+        });
+    
+        await alert.present();
+      }
+    
+      saveUserData(data: any) {
+        const { weightKg, activityMinutes, hydrationGoal } = data;
+    
+        // Save the data to localStorage
+        localStorage.setItem(WEIGHT, String(weightKg));
+        localStorage.setItem(ACTIVE_MINUTES, String(activityMinutes));
+        localStorage.setItem(WATER_GOAL, String(hydrationGoal));
+    
+        // Update the values in the AppStorageService
+        this.appStorage.set(WEIGHT, weightKg);
+        this.appStorage.set(ACTIVE_MINUTES, activityMinutes);
+        this.appStorage.set(WATER_GOAL, hydrationGoal);
+      }
+  
+    toggleTheme() {
+      this.themeService.toggleTheme();
+    }
+  
+}
